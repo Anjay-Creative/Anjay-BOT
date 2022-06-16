ec73354f3ebfbe1c5313a603c63cdeb29b641230
Yan Arie <yanariestradatuhehay@gmail.com>
2022-06-16 23:04:04 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gung enek project kontoldawdaw");
+        System.out.println("Gung enek projejct kontoldawdaw");
     }
 }
