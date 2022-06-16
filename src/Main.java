bd13a11b593d5f836d775363d1ed860c8453a5e2
yanarie123 <yanariestradatuhehay@gmail.com>
2022-06-16 19:39:30 +0700

    Arie keren

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Gung enek project kontol");
+        System.out.println("Gung enek project kontoldawdaw");
     }
 }
